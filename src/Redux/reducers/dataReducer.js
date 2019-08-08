d762c3ded24c3577344553f147ba1d255b6fd5da
Roman <githubkrvchk@gmail.com>
2019-08-08 16:06:16 +0300

    fix bug

--- src/Redux/reducers/dataReducer.js
+++ src/Redux/reducers/dataReducer.js
@@ -5,7 +5,7 @@ import formatingTime from '../../utils/formatingTime';
 
 const initialState = [
     { id: 3, questions: ['hello', 'greeting'], answers: ['greeting', 'hello human'] },
-    { id: 1, questions: ['how are you'], answers: ['i`m fine thank you'] },
+    { id: 1, questions: ['how are you'], answers: ['i am fine thank you'] },
     { id: 5, questions: ['time'], answers: [`it is ${formatingTime()}`] },
     { id: 6, questions: ['thank', 'thanks', 'thank you'], answers: ['You are welcome', 'No thanks you for creation of me - human'] }
 ]
